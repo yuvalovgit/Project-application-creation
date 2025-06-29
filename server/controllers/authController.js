4cdc80b5f6447f9f24865525a0e4c7129b942e2d
Yakirsason <yakirsason111@gmail.com>
2025-06-29 19:31:43 +0300

    setup local mongo and latest register/login fixes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,18 +4,25 @@ const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password, email, fullname } = req.body;
+
     const existingUser = await User.findOne({ username });
     if (existingUser) return res.status(400).json({ error: 'Username already exists' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ username, password: hashedPassword });
+    const newUser = await User.create({ 
+      username, 
+      password: hashedPassword, 
+      email, 
+      fullname 
+    });
 
     res.status(201).json({ message: 'User created', userId: newUser._id });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
 
 exports.login = async (req, res) => {
   try {
