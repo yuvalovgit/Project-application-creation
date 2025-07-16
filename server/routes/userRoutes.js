b75a5ad7718c573c36a4851754044e11ea3de3e3
sahar zada <saharzada7@gmail.com>
2025-07-16 18:07:54 +0300

    Save local changes in feed.html and userRoutes.js before merging dashboard-api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+
 const {
   getUserProfile,
   updateUserProfile,
   followUser,
   getUserPosts,
-  getAllUsers
+  getAllUsers,
+  deleteUser,
 } = require('../controllers/userController');
 
 const authMiddleware = require('../middleware/authMiddleware');
 const upload = require('../middleware/multerConfig');
 const { get } = require('mongoose');
 
+//  /api/users/ and then the rest of the path
+
 router.get('/',authMiddleware, getAllUsers); // Route to get all users for monitoring or admin purposes
 router.get('/:userId', authMiddleware, getUserProfile);
 router.put('/:userId', authMiddleware, upload.single('avatar'), updateUserProfile);
 router.post('/:userId/follow', authMiddleware, followUser);
 router.get('/:userId/posts', authMiddleware, getUserPosts);
+router.delete('/:userId',deleteUser); //deleting a user by ID
 
 module.exports = router;
